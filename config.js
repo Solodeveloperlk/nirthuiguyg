85a3dadfbd4ad27f6816d7324dfa16635a084299
Solo Developer / <nirmalgamagek@gmail.com>
2024-12-18 17:26:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "yqx0gKjZ#nsW9Le4_eyFb3JZfeSicdatTTM-wX1wdHZJ5XTgPYaE",
+SESSION_ID: process.env.SESSION_ID || "wH5ixTxa#sx2cr5qZpW8PlFzkDnil2flVqsQa7UblDlCELKBRzv0",
 MODE : process.env.MODE || "public",// groups/public/private
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
